c48db6efd780dd86d4abc60b4f8ff2fbcf0ccb78
Daniel Gonzalez Reina <dangonrei@gmail.com>
2017-02-13 16:38:07 +0100

    changed tabs for spaces. Everything wokring

--- dblogic.js
+++ dblogic.js
@@ -1,7 +1,7 @@
 //"use strict";
 const mongodb = require("mongodb");
 const encoder = require("custom-encoder")();
-encoder.setBase('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-._~:/?#[]@!$&\'()*+,;=%');
+encoder.setBase('0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_~:/?#[]!$&()+%');
 
 const MongoClient = mongodb.MongoClient;
 const url = process.env.MONGOLAB_URI;
@@ -13,20 +13,23 @@ let collection;
  *
  * @param {function} callback. Is the callback passed to the function to be
  *   executed after the connection to the database has been established. The
- *   callback function is send two params:
+ *   callback function is sent two params:
  *     (database collection, database closing function)
  */
 function connectAndDo(callback) {
-	MongoClient.connect(url, function (err, db) {
-	  if (err) {
-	    console.log('Unable to connect to the mongoDB server. Error:', err);
-	  } else {
-	    console.log('Connection established');
-	    collection = db.collection('url-shortener');
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+      console.log('Connected');
+      collection = db.collection('url-shortener');
 
-	    callback(collection, () => db.close());
-	  }
-	});
+      callback(collection, () => {
+        db.close();
+        console.log('Disconnected');
+      });
+    }
+  });
 }
 
 const api = {};
@@ -40,92 +43,85 @@ const api = {};
  *   process.
  */
 const getNewKey = () => new Promise((resolve, reject) => {
-	connectAndDo((collection, closeDbConnection) => {
-		let newKey;
-		collection.find({_id: 'currentKey'}).toArray()
-		.then(docs => {
-			console.log('currentKey.toArray(): ');
-			console.log(docs);
-			newKey = docs[0].value;
-			newKey++;
+  connectAndDo((collection, closeDbConnection) => {
+    let newKey;
+    collection.find({_id: 'currentKey'}).toArray()
+    .then(docs => {
+      newKey = docs[0].value;
+      newKey++;
 
-			console.log('va a actualizar el currentKey');
-			collection.updateOne({_id: 'currentKey'}, {$set: {value: newKey}})
-			.then(() => {
-				console.log('currentKey gets updated: ' + newKey);
-				resolve(newKey);
-			}
-			,reason => {
-				console.log('There was an error updating currentKey: ');
-				console.log(reason);
-				reject(reason);
-			});
-			
-		},
-		err => {
-			console.log('There was an error trying to read the current key: ');
-			console.log(err);
-			reject(err);
-		});
-	});
+      collection.updateOne({_id: 'currentKey'}, {$set: {value: newKey}})
+      .then(() => {
+        resolve(newKey);
+      }
+      ,reason => {
+        console.log('There was an error updating currentKey: ');
+        console.log(reason);
+        reject(reason);
+      });
+      
+    },
+    err => {
+      console.log('There was an error trying to read the current key: ');
+      console.log(err);
+      reject(err);
+    });
+  });
 });
 
 /**
  * This function stores the url given as param in the database and call the
- * callback giving it the alphanumeric key used to store it.
+ * callback giving it the alphanumeric key used to store it. In the database
+ * it is written a document with `key` being the numeric key (not alphanumeric),
+ * and the `url` being the url.
  *
  * @param {string} url. The url to be stored in the database.
  * @param {function} callback. The function to be called passing the
  *   alphanumeric key used to store the url.
  */
 api.addUrl = (url, callback) => {
-	connectAndDo((collection, closeDbConnection) => {
-		let urlCursor = collection.find({url: url});
-		console.log('urlCursor: ' + urlCursor);
-		console.log('url: ' + url);
+  console.log('addUrl');
+  connectAndDo((collection, closeDbConnection) => {
+    let urlCursor = collection.find({url: url});
 
-		urlCursor.count()
-		.then(count => {
-			console.log('cursor.count = ' + count);
-			if (count !== 0) {
-				// The url was already stored
-				console.log('the url was already stored');
-				
-				urlCursor.toArray()
-				.then(docs => {
-					console.log('cursor.toArray parsed ok');
-					closeDbConnection(); // close the database connection
-					callback(docs[0].key);
-				})
-				.catch(reason => {
-					console.log('cursor.toArray was rejected. Reason: ' + reason);
-				});
+    urlCursor.count()
+    .then(count => {
+      if (count !== 0) {
+        // The url was already stored
+        console.log('the url was already stored');
+        
+        urlCursor.toArray()
+        .then(docs => {
+          closeDbConnection(); // close the database connection
+          callback(docs[0].key);
+        })
+        .catch(reason => {
+          console.log('cursor.toArray was rejected. Reason: ' + reason);
+        });
 
-			} else {
-				// The url must be stored
-				getNewKey()
-				.then(newKey => {
-					console.log('gets the new key: ' + newKey);
+      } else {
+        // The url must be stored
+        getNewKey()
+        .then(newKey => {
 
-					// Store the url linked with the right numeric key
-					connectAndDo((collection, dbClose) => {
-						collection.insertOne({url: url, key: newKey})
-						.then(dbClose);
-					});
+          // Store the url linked with the right numeric key
+          connectAndDo((collection, dbClose) => {
+            collection.insertOne({url: url, key: newKey})
+            .then(dbClose);
+          });
 
-					let encodedKey = encoder.encode(newKey)
-					console.log('codificada: ' + encodedKey);
-					callback(encodedKey);
-				})
-				.catch(reason => {
-					console.log('getNewKey was rejected. Reason: ' + reason);
-				});
-			}
-		})
-		.catch(reason => {
-			console.log('Cursor.count() was rejected. Reason: ' + reason);
-		});
-	});
+          let encodedKey = encoder.encode(newKey)
+          callback(encodedKey);
+        })
+        .catch(reason => {
+          console.log('getNewKey was rejected. Reason: ' + reason);
+        });
+      }
+    })
+    .catch(reason => {
+      console.log('Cursor.count() was rejected. Reason: ' + reason);
+    });
+  });
 }
 
 /**
@@ -139,16 +135,23 @@ api.addUrl = (url, callback) => {
  * @param {function} callback. The function to be called passing the url.
  */
 api.getUrl = (key, callback) => {
-	connectAndDo((collection, closeDbConnection) => {
-		console.log('getUrl');
+  console.log('getUrl');
+  connectAndDo((collection, closeDbConnection) => {
 
-		// TODO pending to write the function
-
-		let content = collection.find();
-		console.log(content);
-
-		closeDbConnection();
-	});
+    collection.find({key: decodedKey}).toArray()
+    .then(docs => {
+      callback(docs[0].url);
+    })
+    .catch(reason => {
+      console.log('There was an error parsing cursor to array. Reason:');
+      console.log(reason);
+      callback(reason);
+    })
+    .then(() => {
+      console.log('cierra la base de datos');
+      closeDbConnection();
+    })
+  });
 };
 
 module.exports = api;
--- server.js
+++ server.js
@@ -5,26 +5,39 @@ const dblogic = require("./dblogic");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.end('Goto /new/:url to insert a new url, or go to /$shortUrl$ to be redirected to the corresponding page');
+  res.end('Goto /new/:url to insert a new url, or go to /$shortUrl$ to be redirected to the corresponding page');
 });
 
 app.get('/new/:url', (req, res) => {
-    //add url
-    dblogic.addUrl(req.params.url, (key) => {
-        // it will call it with the final keyition
-        console.log('Se añade la url en la key: ' + key);
-        res.end('short url: ' + key);
-    });
+  //add url
+  dblogic.addUrl(req.params.url, (key) => {
+    // it will call it with the final keyition
+    console.log('Se añade la url en la key: ' + key);
+    res.end('short url: ' + key);
+  });
 });
 
-app.get('/:pos', (req, res, next) => {
-    //go to pos
-    next();
+/**
+ * Add a specific route to `/favicon.ico` to avoid getting a request and
+ * interpreting it as a `/:key request`, with `key` = "favicon.ico"
+ */
+app.get('/favicon.ico', (req, res) => {
+  // do nothing
+  res.end();
+});
+
+app.get('/:key', (req, res, next) => {
+  console.log('Se llama a get /:key con key: ' + req.params.key);
+  dblogic.getUrl(req.params.key, (url) => {
+    // it should redirect to the url
+    //res.end('url stored: ' + url);
+    res.redirect("http://" +url);
+  });
 });
 
 app.use( (req,res) => {
-    console.log('Entra en el middleware');
-    res.end('Nada por aquí');
+  console.log('Entra en el middleware');
+  res.end('Nada por aquí');
 } );
 
 app.listen(process.env.PORT);
